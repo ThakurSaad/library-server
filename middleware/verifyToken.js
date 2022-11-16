0ad47a1e1acab8d9fe2fb560055cdbfd265f7a5c
ThakurSaad <thakursaad613@gmail.com>
2022-11-16 18:49:53 +0600

    verifyToken

---
+++ middleware/verifyToken.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const { userInfo } = require("os");
+const { promisify } = require("util");
+
+exports.verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers?.authorization?.split(" ")?.[1];
+
+    if (!token) {
+      return res.status(401).json({
+        status: "Fail",
+        error: "You are not logged in",
+      });
+    }
+
+    const decoded = await promisify(jwt.verify)(
+      token,
+      process.env.TOKEN_SECRET
+    );
+    console.log(decoded);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    res.status(403).json({
+      status: "Fail",
+      error: "Invalid Token",
+      error: error.message,
+    });
+  }
+};
