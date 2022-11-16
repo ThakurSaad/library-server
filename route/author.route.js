a30e1d889b0f317ab8dd83519eeb9fd13eef4d66
ThakurSaad <thakursaad613@gmail.com>
2022-11-16 19:57:46 +0600

    route authenticated

--- route/author.route.js
+++ route/author.route.js
@@ -6,13 +6,13 @@ const { verifyToken } = require("../middleware/verifyToken");
 router
   .route("/")
   .post(authorController.createAuthors)
-  .get(authorController.getAuthors);
+  .get(verifyToken, authorController.getAuthors);
 
 router.get("/me", verifyToken, authorController.getAuthorDetails);
 
 router
   .route("/:id")
-  .get(authorController.getAuthorById)
+  .get(verifyToken, authorController.getAuthorById)
   .delete(authorController.deleteAuthorById)
   .patch(authorController.updateAuthorById);
 
--- route/book.route.js
+++ route/book.route.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const bookController = require("../controller/book.controller");
+const { verifyToken } = require("../middleware/verifyToken");
 
-router.route("/").post(bookController.createBooks).get(bookController.getBooks);
+router
+  .route("/")
+  .post(bookController.createBooks)
+  .get(verifyToken, bookController.getBooks);
 
-router.put("/like/:id", bookController.likeBookById);
+router.put("/like/:id", verifyToken, bookController.likeBookById);
 
-router.put("/unlike/:id", bookController.unlikeBookById);
+router.put("/unlike/:id", verifyToken, bookController.unlikeBookById);
 
 router
   .route("/:id")
